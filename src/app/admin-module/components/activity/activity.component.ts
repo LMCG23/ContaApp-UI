c54c41d3026cf47ba66f6adc0b5143ec3f3ebfb9
LMCG23 <leinercg23@outlook.com>
2022-12-08 16:04:17 -0600

    list of activities created FE

--- src/app/admin-module/components/activity/activity.component.ts
+++ src/app/admin-module/components/activity/activity.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Employee, Product, TableRows, TopSelling } from '../../../component/table/table-data';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Activity } from "../../../shared/models/Activity";
+import { environment } from "../../../../environments/environment";
+import {
+  Employee,
+  Product,
+  TableRows,
+  TopSelling,
+} from "../../../component/table/table-data";
 
 @Component({
-  selector: 'app-activity',
-  templateUrl: './activity.component.html',
-  styleUrls: ['./activity.component.scss']
+  selector: "app-activity",
+  templateUrl: "./activity.component.html",
+  styleUrls: ["./activity.component.scss"],
 })
 export class ActivityComponent implements OnInit {
-  topSelling:Product[];
+  topSelling: Product[];
 
-  trow:TableRows  [];
+  trow: TableRows[];
 
+  //List of Activities
+  public activityList: Array<Activity> = new Array<Activity>();
+
+  //Variables
+  accountingId: number;
   constructor() {
+    this.accountingId = parseInt(
+      localStorage.getItem(environment.localStorage.userAccountingFirm) || ""
+    );
+    if (this.accountingId) {
+      // this.fetchActivityByAccountingId();
+    }
+    this.topSelling = TopSelling;
 
-    this.topSelling=TopSelling;
-
-    this.trow=Employee;
+    this.trow = Employee;
   }
 
-ngOnInit(): void {
+  ngOnInit(): void {}
 
-}
-
+  // fetchActivityByAccountingId() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
 import {
-  CommonModule, LocationStrategy,
-  PathLocationStrategy
-} from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PerfectScrollbarConfigInterface, PerfectScrollbarModule, PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { Approutes } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FullComponent } from './layouts/full/full.component';
-import { NavigationComponent } from './shared/header/navigation.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { SpinnerComponent } from './shared/spinner.component';
-
-
+  CommonModule,
+  LocationStrategy,
+  PathLocationStrategy,
+} from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import {
+  PerfectScrollbarConfigInterface,
+  PerfectScrollbarModule,
+  PERFECT_SCROLLBAR_CONFIG,
+} from "ngx-perfect-scrollbar";
+import { Approutes } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FullComponent } from "./layouts/full/full.component";
+import { NavigationComponent } from "./shared/header/navigation.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { SpinnerComponent } from "./shared/spinner.component";
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
   wheelSpeed: 1,
   wheelPropagation: true,
-  minScrollbarLength: 20
+  minScrollbarLength: 20,
 };
 
 @NgModule({
@@ -42,20 +45,23 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule,
-    RouterModule.forRoot(Approutes, { useHash: false, relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(Approutes, {
+      useHash: false,
+      relativeLinkResolution: "legacy",
+    }),
     PerfectScrollbarModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     {
       provide: LocationStrategy,
-      useClass: PathLocationStrategy
+      useClass: PathLocationStrategy,
     },
     {
       provide: PERFECT_SCROLLBAR_CONFIG,
-      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-    }
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
